863650e0ee3565bbd76593b9805630e7c58573a1
Jessie Wright <jessie.wright@gmail.com>
2020-04-09 19:42:01 +0000

    fixed linting problems

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 const breed = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if(error) {
+  if (error) {
     console.log('error:', error); // Print the error if one occurred
     process.exit();
   }
