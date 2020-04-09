ba8b02b8e96938a7e2d2cd9dcebf8b9a6b32e6f3
Jessie Wright <jessie.wright@gmail.com>
2020-04-09 19:41:10 +0000

    implemented breed fetch and description printing, with rudimentary error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
 const request = require('request');
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if(error) {
+    console.log('error:', error); // Print the error if one occurred
+    process.exit();
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log('no data returned for provided breed');
+  } else {
+    console.log(data[0].description);
+  }
+});
